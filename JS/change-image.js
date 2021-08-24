43cabd63841d899ae7b6c564ed06e791ee3a66ab
klesio25 <klesio.k9@gmail.com>
2021-08-23 21:58:48 -0300

    added new images and new features in dark mode

--- JS/change-image.js
+++ JS/change-image.js
@@ -1,33 +1,15 @@
-var img = document.getElementById("icon-lamp").src;
-var imgOn = `img/lampada-on.png`;
-var imgOff = `img/lampada-off.png`;
-var acender = true;
-var apagar = false;
-var count = 0;
+let darkMode = document.querySelector("#active-dark-mode");
+let imgName = document.querySelector("#icon-lamp");
+let logo = document.querySelector("#logo-dark-mode");
 
-function status() {
-  count++;
+darkMode.onclick = function () {
 
-  console.log(count);
-
-  if (count % 2 == 0) {
-    return acender;
+  if (imgName.src.match("off")) {
+    imgName.src = "img/lampada-on.png";
+    logo.src = "img/instagram-logo3.png"
   } else {
-    return apagar;
+    imgName.src = "img/lampada-off.png";
+    logo.src = "img/instagram-logo.png"
   }
 
-}
-console.log(count);
-
-console.log(status());
-
-function onOff() {
-  if (status() != true) {
-    document.getElementById("icon-lamp").src = imgOff;
-    apagar = false;
-  } else {
-    document.getElementById("icon-lamp").src = imgOn;
-  }
-}
-
-onOff();
+};
